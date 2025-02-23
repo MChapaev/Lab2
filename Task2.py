c5ea3de0af47521cb7624f95eb5b0bd89c3338aa
SameGevDudios <magomed.chapaev1@mail.ru>
2025-02-23 18:06:39 +0300

    Add Task 2
    Task 2 is to analyze an tree and print 1 if the first element
    is an ancestor of the second, 2 if the second
    is an ancestor of the first, or 0 if none of them is an ancestor of the other

--- Task2.py
+++ Task2.py
@@ -1,10 +1,57 @@
+def build_genealogy_tree(pairs):
+    tree = {}
+    for child, parent in pairs:
+        tree[child] = parent
+        if parent not in tree:
+            tree[parent] = None
 
+    return tree
+
+
+def is_ancestor(tree, ancestor, descendant):
+    while descendant in tree and tree[descendant] is not None:
+        if tree[descendant] == ancestor:
+            return True
+        descendant = tree[descendant]
+    return False
+
+
+def process_queries(tree, queries):
+    results = []
+    for a, b in queries:
+        if is_ancestor(tree, a, b):
+            results.append(1)
+        elif is_ancestor(tree, b, a):
+            results.append(2)
+        else:
+            results.append(0)
+    return results
 
 
 # Main
 def main():
     print('Provide multiline (empty line to stop):')
+    data = [
+        ("Alexei", "Peter_I"),
+        ("Anna", "Peter_I"),
+        ("Elizabeth", "Peter_I"),
+        ("Peter_II", "Alexei"),
+        ("Peter_III", "Anna"),
+        ("Paul_I", "Peter_III"),
+        ("Alexander_I", "Paul_I"),
+        ("Nicholaus_I", "Paul_I")
+    ]
 
+    queries = [
+        ("Peter_I", "Paul_I"),
+        ("Paul_I", "Peter_I"),
+        ("Alexei", "Elizabeth")
+    ]
+
+    tree = build_genealogy_tree(data)
+    results = process_queries(tree, queries)
+    for res in results:
+        print(res)
 
 
 if __name__ == '__main__':
